f0a6f59ba2bd31214caa163723ac86e0efbdc5ca
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2023-09-22 17:51:08 +0330

    moved the api key to the env file

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false;
 
 axios.defaults.baseURL = 'https://exchange-rate9.p.rapidapi.com';
 //TODO: change authorization method
-axios.defaults.headers['X-RapidAPI-Key'] = '';
+axios.defaults.headers['X-RapidAPI-Key'] = process.env.VUE_APP_X_RAPIDAPI_KEY;
 axios.defaults.headers['X-RapidAPI-Host'] = 'exchange-rate9.p.rapidapi.com';
 
 Vue.prototype.$http = axios;
