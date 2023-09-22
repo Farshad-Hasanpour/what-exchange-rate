993861e295bb83213b62dc07f7972c6f4a13454c
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2023-09-22 13:38:06 +0330

    updated the app

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,11 @@
 		<div id="app">
 			<Snackbar/>
 			<header>
-				<a href="/">
-					<h1 class="m-0" style="color: var(--color-primary); line-height: 1;">What Exchange Rate</h1>
+				<a href="/" style="margin-right: auto;">
+					<h1>What Exchange Rate</h1>
 				</a>
-				<div class="actions">
 					<Button
-						class="action-button"
+						class="mx-1"
 						type="fab"
 						dark
 						height="36px"
@@ -22,7 +21,7 @@
 						href="https://rapidapi.com/Serply/api/exchange-rate9/"
 						target="_blank"
 						rel="noopener"
-						class="action-button"
+						class="mx-1"
 					>
 						<Button
 							type="fab"
@@ -38,7 +37,7 @@
 						href="https://github.com/Farshad-Hasanpour/what-exchange-rate"
 						target="_blank"
 						rel="noopener"
-						class="action-button"
+						class="mx-1"
 					>
 						<Button
 							type="fab"
@@ -51,7 +50,7 @@
 						</Button>
 					</a>
 					<!--				<Button-->
-					<!--					class="action-button"-->
+					<!--					class="mx-1"-->
 					<!--					type="fab"-->
 					<!--					dark-->
 					<!--					height="36px"-->
@@ -61,57 +60,51 @@
 					<!--				>-->
 					<!--					<Icon name="translate"/>-->
 					<!--				</Button>-->
-				</div>
 			</header>
 			<main>
-				<section>
-					<transition-group tag="div" id="selected-rows" name="card-fade">
-						<SelectedCard
-							v-for="rate in selectedRates"
-							:key="rate.id"
-							:title="rate.name"
-							:description="rate.value"
-						/>
-					</transition-group>
-				</section>
-				<section style="flex-grow: 1">
-					<div class="section-header">
-						<h2 style="font-weight: normal; font-size: 1.5em; line-height: 1;">
-							Exchange Rates Based on
-							<span class="base-select-container">
-								<select v-model="base" class="base-select-input">
-									<option
-										v-for="({description}, symbol) in symbols"
-										:key="symbol"
-										:value="symbol"
-									>
-										{{description}}
-									</option>
-								</select>
-							</span>
-
-						</h2>
-					</div>
-					<Table
-							v-model="selected"
-							:items="ratesToShow"
-							:selectable="true"
-							:max-select-count="4"
-							:headers="headers"
-							:loading="fetchingRates || fetchingSymbols"
-							:actions="actions"
-							@searchOnGoogle="searchOnGoogle($event)"
-							@setAsBase="base=$event"
+				<transition-group tag="div" id="selected-rows" name="card-fade">
+					<SelectedCard
+						v-for="rate in selectedRates"
+						:key="rate.id"
+						:title="rate.name"
+						:description="rate.value"
 					/>
-				</section>
-				<footer>
-					<p style="display: flex; align-items: center;">
-						<span>Made with</span>
-						<Icon name="heart" style="color: red; margin: 0 4px; font-size: 18px;"/>
-						<span>by <a href="https://www.linkedin.com/in/farshad-hasanpour/" target="_blank" rel="noopener">Farshad Hasanpour</a></span>
-					</p>
-				</footer>
+				</transition-group>
+				<h2 class="mx-1 my-5 relative">
+					Exchange Rates Based on
+					<select v-if="symbols.length" v-model="base" class="base-select-input">
+						<option
+							v-for="({description}, symbol) in symbols"
+							:key="symbol"
+							:value="symbol"
+						>
+							{{description}}
+						</option>
+					</select>
+					<span v-else>United States Dollar</span>
+				</h2>
+				<Table
+					v-model="selected"
+					:items="ratesToShow"
+					:selectable="true"
+					:max-select-count="4"
+					:headers="headers"
+					:loading="fetchingRates || fetchingSymbols"
+					:actions="actions"
+					class="flex-grow-1"
+					@searchOnGoogle="searchOnGoogle($event)"
+					@setAsBase="base=$event"
+				/>
 			</main>
+			<footer>
+				<p class="flex aic">
+					<span>Made with</span>
+					<Icon name="heart" color="red" size="18px" class="mx-1 my-0"/>
+					<span>by <a href="https://www.linkedin.com/in/farshad-hasanpour/" target="_blank" rel="noopener">Farshad Hasanpour</a></span>
+				</p>
+				<p>Foreign exchange rates published by the European Central Bank</p>
+				<p><b>DISCLAIMER:</b> Do not use this website for financial decision making.</p>
+			</footer>
 		</div>
 	</div>
 </template>
@@ -227,134 +220,11 @@
 </script>
 
 <style>
+	@import "./assets/css/reset.css";
+	@import "./assets/css/themes.css";
+	@import "./assets/css/default.css";
 	@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
 	@import '../node_modules/@mdi/font/css/materialdesignicons.min.css';
-
-	:root{
-		--breakpoint-mobile: 600px;
-		--breakpoint-small-tablet: 960px;
-		--breakpoint-large-tablet: 1264px;
-		--breakpoint-desktop: 1904px;
-
-		--animation-speed: 0.5s;
-	}
-
-	#app-wrapper[theme="light"]{
-		--color-primary: #1A237E;
-		--color-secondary: #F1F2F6;
-		--color-success: #4ab190;
-		--color-error: #ef3d59;
-		--color-text: #2c3e50;
-
-		--color-card-back: #FFF;
-		--color-line: rgba(0, 0, 0, .1);
-
-	}
-
-	#app-wrapper[theme="dark"]{
-		--color-primary: #7989DA;
-		--color-secondary: #171C21;
-		--color-success: #4ab190;
-		--color-error: #ef3d59;
-		--color-text: #FFF;
-
-		--color-card-back: #2F3841;
-		--color-line: rgba(255, 255, 255, .1);
-	}
-
-	#app-wrapper{
-		font-family: Nunito, sans-serif;
-		position: fixed;
-		height: 100%;
-		width: 100%;
-		color: var(--color-text);
-		background-color: var(--color-secondary);
-	}
-
-	#app-wrapper[rtl] #app{
-		direction: rtl;
-		text-align: right;
-	}
-	#app { height: 100%;}
-
-	/* style reset */
-	*{ box-sizing: border-box;}
-	body{ margin: 0; padding: 0; }
-	ul, li{
-		margin: 0;
-		padding: 0;
-		list-style: none;
-	}
-	a,
-	a:visited,
-	a:active,
-	a:hover,
-	a:focus{
-		text-decoration: none;
-		color: var(--color-primary);
-	}
-	section{ margin-bottom: 16px; }
-	header{
-		width: 100%;
-		padding: 0 25px;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		height: 70px;
-		position: fixed;
-		top: 0;
-		left: 0;
-		right: 0;
-		background-color: var(--color-secondary);
-		box-shadow: var(--color-line) 0px 2px 8px 0px;
-		z-index: 100;
-	}
-	header > .actions > .action-button:not(:first-child){
-		margin-inline-start: 15px;
-	}
-	main{
-		padding-top: 20px;
-		padding-left: 25px;
-		padding-right: 25px;
-		padding-bottom: 0;
-		scroll-behavior: smooth;
-		overflow-y: auto;
-		margin-top: 70px;
-		height: calc(100% - 70px);
-		width: 100%;
-		display: flex;
-		flex-direction: column;
-	}
-
-	/*** Chrome Scroll ***/
-	::-webkit-scrollbar {
-		width: 12px;
-		background: var(--color-secondary);
-	}
-
-	::-webkit-scrollbar-track {
-		-webkit-box-shadow: inset 0 0 8px rgba(0,0,0,0.5);
-	}
-
-	::-webkit-scrollbar-thumb {
-		background: #888;
-		border-radius: 10px;
-	}
-
-	::-webkit-scrollbar-thumb:hover{
-		background-color: #666;
-	}
-	/*** Chrome Scroll End ***/
-
-	.section-header{
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-start;
-		align-items: center;
-	}
-	.section-header > *:not(:last-child){
-		margin-inline-end: 20px;
-	}
 
 	#selected-rows{
 		display: flex;
@@ -364,72 +234,4 @@
 		margin-right: -10px;
 		margin-left: -10px;
 	}
-
-	.card-fade-enter-active {
-		transition: opacity .3s;
-	}
-	.card-fade-leave-active{
-		opacity: 0;
-		position:absolute;
-	}
-	.card-fade-enter, .cardfade-leave-to {
-		opacity: 0;
-	}
-	.card-fade-move{
-		transition: transform .3s;
-	}
-
-	.no-symbol-selected{
-		text-align: center;
-		margin-bottom: 0;
-		color: var(--color-error);
-	}
-
-	.m-0{
-		margin: 0;
-	}
-
-	.base-select-input{
-		appearance: none;
-		-webkit-appearance: none;
-		-moz-appearance: none;
-		outline: none;
-		border: none;
-		background-color: var(--color-card-back);
-		color: white;
-		height: 36px;
-		border-radius: 4px;
-		font-size: 16px;
-		padding-top: 0;
-		padding-bottom: 0;
-		padding-left: 12px;
-		padding-right: 36px;
-		width: 350px;
-	}
-	/* Style for the arrow icon */
-	.base-select-container{
-		display: inline-block;
-		position: relative;
-	}
-	.base-select-container::after {
-		content: '\25BC';
-		position: absolute;
-		top: 50%;
-		right: calc((36px - 14px) / 2);
-		transform: translateY(-50%);
-		font-size: 14px;
-		width: 14px;
-		height: 14px;
-		pointer-events: none; /* Prevents clicking on the arrow */
-	}
-
-	footer{
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		height: 70px;
-		padding: 0 25px;
-		margin-top: auto;
-	}
-
 </style>
